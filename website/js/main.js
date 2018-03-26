0c8b2498ec53494c3d4c2ba5e47fe2290b6354e3
Berend Kalberg <berend.kalberg@gmail.com>
2018-03-26 11:53:58 +0200

    Veel gedaan met api

--- website/js/main.js
+++ website/js/main.js
@@ -6,3 +6,28 @@
     
     }); // end of document ready
     })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    $("#getWeatherForcast").click(function() {
+        var city = $("#city").val();
+        var key = '8877070b6d5e68288c1f3a0dbac54ef6';
+
+        $.ajax({
+            url: '',
+        });
+    });
+});
+
+fetch('/events')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+        var arrayLength = myJson.length;
+        for (var i = 0; i < arrayLength; i++) {
+            var div = document.createElement("div");       
+            div.innerHTML = myJson[i].name + " " + myJson[i].start.month + "-" + myJson[i].start.day + " Dit begint om: " + myJson[i].start.hour + ":00"              
+            document.getElementById("holder").appendChild(div);                   
+    }
+  });
