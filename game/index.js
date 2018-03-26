0fac6418c685f9597a8632f97d760cdcbb335c2d
boltgolt <boltgolt@gmail.com>
2018-03-26 10:02:23 +0200

    Socket server + client

---
+++ game/index.js
@@ -0,0 +1,7 @@
+var socket = require("socket.io-client")("http://localhost:4200")
+
+socket.on("connect", function() {
+	console.log("Connected to socket")
+
+	socket.emit("hello", "gaem")
+})
---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const server = http.createServer(app)
+const io = require("socket.io")(server)
+
+app.use(express.static(__dirname + "/../website"))
+
+app.get("/events", function(req, res){
+	console.log("Refreshing raw API data")
+	http.get("http://stadspodium-rotterdam.nl/wp-json/tribe/events/v1/events?start_date=2017-03-13%2000:59:00", sRes => {
+		sRes.setEncoding("utf8")
+		let body = ""
+
+		sRes.on("data", data => {
+			body += data
+		})
+
+		sRes.on("end", () => {
+			let json = JSON.parse(body)
+			let output = []
+
+			console.log("Parsing API data")
+
+			for (let event of json["events"]) {
+				if (event.id == 2481) continue
+
+				output.push({
+					"id": event.id,
+					"name": event.title,
+					"start": event.start_date_details
+				})
+			}
+
+			res.send(output)
+		})
+	})
+})
+
+io.on("connection", function(socket) {
+	let clientType = false
+
+	socket.on("hello", (type) => {
+		clientType = type
+		console.log("Socket from ", clientType, " type accepted")
+	})
+
+	socket.on("disconnect", () => {
+		console.log("Socket from ", clientType, " type lost")
+	})
+})
+
+server.listen(4200)
+
+console.log("Server running on http://localhost:4200")
