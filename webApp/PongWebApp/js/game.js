dcd231b2417c6965780c699f8bc16397b13503f4
DionGrimm <31693758+DionGrimm@users.noreply.github.com>
2018-03-26 11:48:48 +0200

    update webapp

--- webApp/PongWebApp/js/game.js
+++ webApp/PongWebApp/js/game.js
@@ -27,70 +27,36 @@ var ctx = c.getContext("2d");
 ctx.fillStyle="#FFFFFF"
 ctx.strokeStyle="#000000"
 
-for (var i = 0; i < height/10; i++) {
+// Animation frame
+function animate() {
+    ctx.clearRect(0,0,width,height);
+    ctx.font = "30px FFFForward"
+
+    ctx.fillRect(xP1,yP1,widthP,heightP);
+    ctx.stroke();
+    
+    ctx.fillRect(xP2,yP2,widthP,heightP);
+    ctx.stroke();
+    
+    ctx.fillRect(xBall,yBall,sizeBall,sizeBall);
+    ctx.stroke();
+    
+    ctx.fillText(scoreP1,width/2-52,50);
+    ctx.fillText(scoreP2,width/2+30,50);
+    
+    // Walls
+    for (var i = 0; i < height/10; i++) {
     ctx.fillRect(xWall,i*sizeWall*1.5-sizeWall*0.5,sizeWall,sizeWall);
     ctx.stroke();
-}
-
-// Animation frame
-var stop = false;
-var frameCount = 0;
-var $results = $("#results");
-var fps = 60;
-var fpsInterval, startTime, now, then, elapsed;
-
-
-// initialize the timer variables and start the animation
-
-function startAnimating(fps) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    animate();
-}
-
-// the animation loop calculates time elapsed since the last loop
-// and only draws if your specified fps interval is achieved
-
-function animate() {
-
-    // request another frame
-
-    requestAnimationFrame(animate);
-
-    // calc elapsed time since last loop
-
-    now = Date.now();
-    elapsed = now - then;
-
-    // if enough time has elapsed, draw the next frame
-
-    if (elapsed > fpsInterval) {
-
-        // Get ready for next frame by setting then=now, but also adjust for your
-        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
-        then = now - (elapsed % fpsInterval);
-
-        // Put your drawing code here
-        ctx.font = "30px FFFForward"
-
-        ctx.fillRect(xP1,yP1,widthP,heightP);
-        ctx.stroke();
-
-        ctx.fillRect(xP2,yP2,widthP,heightP);
-        ctx.stroke();
-
-        ctx.fillRect(xBall,yBall,sizeBall,sizeBall);
-        ctx.stroke();
-
-        ctx.fillText(scoreP1,width/2-52,50);
-        ctx.fillText(scoreP2,width/2+30,50);
-
-        // Countdown
-        if (countdown > 0) {
-            ctx.font = "100px FFFForward"
-            ctx.fillText(countdown,width/2-30,height/2+50);
-            ctx.strokeText(countdown,width/2-30,height/2+50);
-        }
     }
-}
+
+    // Countdown
+    if (countdown > 0) {
+        ctx.font = "100px FFFForward"
+        ctx.fillText(countdown,width/2-30,height/2+50);
+        ctx.strokeText(countdown,width/2-30,height/2+50);
+    }
+    window.requestAnimationFrame(animate);
+  }
+
+window.requestAnimationFrame(animate);
--- webApp/PongWebApp/js/main.js
+++ webApp/PongWebApp/js/main.js
@@ -7,69 +7,79 @@ var height = width*0.75;
 var headerHeight = height/3;
 var footerHeight = height*2/3
 
-document.getElementById("wrapperHeader").style.height = headerHeight + "px";
-var canvasCount = document.getElementsByClassName("wrapperCanvas");
-for (i = 0; i < canvasCount.length; i++) {
-    canvasCount[i].style.height = height + "px";
-    canvasCount[i].style.display = "none";
-} 
-var footerCount = document.getElementsByClassName("wrapperFooter");
-for (i = 0; i < footerCount.length; i++) {
-    footerCount[i].style.height = footerHeight + "px";
-    footerCount[i].style.display = "none";
-} 
+var appStatus = "mainMenu";
+var gameMode = 0;
 
 c.width = width;
 c.height = height;
 
-// App menus
-var appStatus = mainMenu;
-var gameMode = 0;
+function updateScreen() {
+    document.getElementById("wrapperHeader").style.height = headerHeight + "px";
+    var canvasCount = document.getElementsByClassName("wrapperCanvas");
+    for (i = 0; i < canvasCount.length; i++) {
+        canvasCount[i].style.height = height + "px";
+        canvasCount[i].style.display = "none";
+    } 
+    var footerCount = document.getElementsByClassName("wrapperFooter");
+    for (i = 0; i < footerCount.length; i++) {
+        footerCount[i].style.height = footerHeight + "px";
+        footerCount[i].style.display = "none";
+    } 
 
-switch (appStatus) {
-    case mainMenu:
-        document.getElementById("mainMenu").style.display = "flex";
-        document.getElementById("playButton").style.display = "flex";
-        document.getElementById("playButton").addEventListener("click", function(){
-            appStatus = chooseMode;
-            console.log("test");
-        }, false)
-        break;
+    // App menus
+    switch (appStatus) {
+        case "mainMenu":
+            document.getElementById("mainMenu").style.display = "flex";
+            document.getElementById("playButton").style.display = "flex";
+            document.getElementById("playButton").addEventListener("click", function(){
+                console.log("test");
+                socket.emit("switchStatus","chooseMode")
+            }, false)
+            break;
 
-    case chooseMode:
-        document.getElementById("chooseMode").style.display = "flex";
-        document.getElementById("gamemodeButtons").style.display = "flex";
-        document.getElementById("classic").addEventListener("click", function(){
-            appStatus = game;
-            gameMode = 0;
-            console.log("test");
-        }, false)
-        document.getElementById("coop").addEventListener("click", function(){
-            appStatus = game;
-            gameMode = 1;
-            console.log("test");
-        }, false)
-        break;
+        case "chooseMode":
+            document.getElementById("chooseMode").style.display = "flex";
+            document.getElementById("gamemodeButtons").style.display = "flex";
+            document.getElementById("classic").addEventListener("click", function(){
+                gameMode = 0;
+                console.log("test");
+                socket.emit("switchStatus","game")
+            }, false)
+            document.getElementById("coop").addEventListener("click", function(){
+                gameMode = 1;
+                console.log("test");
+                socket.emit("switchStatus","game")
+            }, false)
+            break;
 
-    case game:
-        document.getElementById("game").style.display = "flex";
-        document.getElementById("playButton").style.display = "flex";
-        break;
+        case "game":
+            document.getElementById("game").style.display = "flex";
+            document.getElementById("playButton").style.display = "flex";
+            break;
 
-    case nameInput:
-        document.getElementById("nameInput").style.display = "flex";
-        document.getElementById("playButton").style.display = "flex";
-        break;
+        case "nameInput":
+            document.getElementById("nameInput").style.display = "flex";
+            document.getElementById("playButton").style.display = "flex";
+            break;
 
-    case scoreMenu:
-        document.getElementById("scoreMenu").style.display = "flex";
-        document.getElementById("playButton").style.display = "flex";
-        document.getElementById("playButton").addEventListener("click", function(){
-            appStatus = chooseMode;
-            console.log("test");
-        }, false)
-        break;
+        case "scoreMenu":
+            document.getElementById("scoreMenu").style.display = "flex";
+            document.getElementById("playButton").style.display = "flex";
+            document.getElementById("playButton").addEventListener("click", function(){
+                console.log("test");
+                socket.emit("switchStatus","mainMenu")
+            }, false)
+            break;
 
-    default:
-        break;
+        default:
+            break;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    socket.on("switchStatus", function(status) {
+        appStatus = status;
+        updateScreen();
+    });
+});
+updateScreen();
